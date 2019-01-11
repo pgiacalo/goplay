0c8ee161ab2ec1b94b0079d1ddd22635c7161268
Philip Giacalone <philip_giacalone@yahoo.com>
2019-01-10 18:34:01 -0800

    Added TimeDate.go

--- AdsbClient.go
+++ AdsbClient.go
@@ -12,8 +12,11 @@ import (
 )
 
 /*
+	see: http://woodair.net/sbs/article/barebones42_socket_data.htm
+
 	This struct holds the data received from aircraft via ADS-B messages (ADSB).
-	It is received in a format called "SBS1 (BaseStation) format".
+
+	It is received in a format called "SBS1 (BaseStation, beast) format".
 	These ADSB messages are provided by the dump1090 program and are available on port 30003 (by default).
 	Messages (MSG) received from aircraft may be one of eight types.
 
@@ -26,6 +29,19 @@ import (
 	MSG,3,5,211,4CA2D6,10057,2008/11/28,14:53:50.594,2008/11/28,14:58:51.153,,37000,,,51.45735,-1.02826,,,0,0,0,0
 	MSG,8,5,812,ABBEE3,10095,2008/11/28,14:53:50.594,2008/11/28,14:58:51.153,,,,,,,,,,,,0
 
+	SEL,,496,2286,4CA4E5,27215,2010/02/19,18:06:07.710,2010/02/19,18:06:07.710,RYR1427
+	ID,,496,7162,405637,27928,2010/02/19,18:06:07.115,2010/02/19,18:06:07.115,EZY691A
+	AIR,,496,5906,400F01,27931,2010/02/19,18:06:07.128,2010/02/19,18:06:07.128
+	STA,,5,179,400AE7,10103,2008/11/28,14:58:51.153,2008/11/28,14:58:51.153,RM
+	CLK,,496,-1,,-1,2010/02/19,18:18:19.036,2010/02/19,18:18:19.036
+	MSG,1,145,256,7404F2,11267,2008/11/28,23:48:18.611,2008/11/28,23:53:19.161,RJA1118,,,,,,,,,,,
+	MSG,2,496,603,400CB6,13168,2008/10/13,12:24:32.414,2008/10/13,12:28:52.074,,,0,76.4,258.3,54.05735,-4.38826,,,,,,0
+	MSG,3,496,211,4CA2D6,10057,2008/11/28,14:53:50.594,2008/11/28,14:58:51.153,,37000,,,51.45735,-1.02826,,,0,0,0,0
+	MSG,4,496,469,4CA767,27854,2010/02/19,17:58:13.039,2010/02/19,17:58:13.368,,,288.6,103.2,,,-832,,,,,
+	MSG,5,496,329,394A65,27868,2010/02/19,17:58:12.644,2010/02/19,17:58:13.368,,10000,,,,,,,0,,0,0
+	MSG,6,496,237,4CA215,27864,2010/02/19,17:58:12.846,2010/02/19,17:58:13.368,,33325,,,,,,0271,0,0,0,0
+	MSG,7,496,742,51106E,27929,2011/03/06,07:57:36.523,2011/03/06,07:57:37.054,,3775,,,,,,,,,,0
+	MSG,8,496,194,405F4E,27884,2010/02/19,17:58:13.244,2010/02/19,17:58:13.368,,,,,,,,,,,,0
 */
 
 /*
@@ -34,7 +50,7 @@ import (
 	Depending of the Transmission Type, some of the fields will be empty.
 */
 type adsbMsg struct {
-	MessageType          string  //Field 1: (MSG STA ID AIR SEL or CLK)
+	MessageType          string  //Field 1: (MSG, STA, ID, AIR, SEL, or CLK)
 	TransmissionType     int     //Field 2: MSG sub types 1 to 8. Not used by other message types.
 	SessionID            int     //Field 3: Database Session record number
 	AircraftID           string  //Field 4: Database Aircraft record number
---
+++ TimeDate.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	start := time.Now()
+	//Get the abbreviated name of the time zone (such as "CET") and its offset in seconds east of UTC.
+	fmt.Println(start.Zone())	//returns PST -28800
+
+	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
+	t, _ := time.Parse(longForm, "Feb 3, 2013 at 7:54pm (PST)")
+	fmt.Println(t)
+
+	const shortForm = "2006-Jan-02"
+	t, _ = time.Parse(shortForm, "2013-Feb-03")
+	fmt.Println(t)
+
+ 	//loc, _ := time.LoadLocation("Europe/Berlin") 		//e.g., "America/New_York"
+	loc, _ := time.LoadLocation(time.Now().Location().String())
+
+	t, _ = time.ParseInLocation(longForm, "Jul 9, 2012 at 5:02am (CEST)", loc)
+	fmt.Println(t)
+
+	// Note: without explicit zone, returns time in given location.
+	t, _ = time.ParseInLocation(shortForm, "2012-Jul-09", loc)
+	fmt.Println(t)
+
+	//easy time zone conversions
+	timeHere := time.Now()
+	fmt.Printf("Time here=%v\n", timeHere.String())
+
+	loc, _ = time.LoadLocation("UTC")
+	utcTime := timeHere.In(loc)
+	fmt.Printf("Time UTC =%v\n", utcTime)
+
+	end := time.Now()
+	diff := end.Sub(start)
+	fmt.Printf("Runtime=%v\n", diff.String())
+
+} //end main()
+
+
+
