547310d575186fdab7670f530e7ac2e77f5ab9d5
Philip Giacalone <philip_giacalone@yahoo.com>
2021-10-18 20:28:40 -0700

    Gave functions better names

--- Closures.go
+++ Closures.go
@@ -5,25 +5,24 @@ import (
 )
 
 func main() {
-	b := html_tag("<b>", "</b>")
-	i := html_tag("<i>", "</i>")
-	p := html_tag("<p>", "</p>")
+	b := html_tagger("<b>", "</b>")
+	i := html_tagger("<i>", "</i>")
+	p := html_tagger("<p>", "</p>")
 	fmt.Println(b("bold"))      //<b>bold</b>
 	fmt.Println(i("italic"))    //<i>italic</i>
 	fmt.Println(p("paragraph")) //<p>paragraph</p>
 
-	c1 := count(0)
-	c2 := count(10)
-	fmt.Println(c1(), c1()) //1 2
-	fmt.Println(c1(), c1()) //3 4
-	fmt.Println(c2(), c2()) //11 12
+	c1 := counter(1)
+	c2 := counter(10)
+	fmt.Println(c1(), c1(), c1(), c1()) //1 2 3 4
+	fmt.Println(c2(), c2())             //10 11
 
 	sum := adder()
 	fmt.Println(sum(5), sum(10)) //5 15
 
 }
 
-func html_tag(opentag string, closetag string) func(string) string {
+func html_tagger(opentag string, closetag string) func(string) string {
 	//definition: 	a closure is a function that remembers the variable(s)
 	//				from the environment in which it was created.
 	f := func(str string) string {
@@ -32,12 +31,13 @@ func html_tag(opentag string, closetag string) func(string) string {
 	return f
 }
 
-func count(start int) func() int {
+func counter(start int) func() int {
 	//definition: 	a closure is a function that remembers the variable(s)
 	//				from the environment in which it was created.
+	count := start - 1
 	f := func() int {
-		start++
-		return start
+		count++
+		return count
 	}
 	return f
 }
